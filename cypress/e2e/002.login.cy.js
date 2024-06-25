e867d8a65db577d71595306ae229fc88e0649c67
mijailaoh <mijail.aoh@gmail.com>
2024-06-25 00:36:31 -0400

    Update Test case 002

--- cypress/e2e/002.login.cy.js
+++ cypress/e2e/002.login.cy.js
@@ -34,17 +34,47 @@ describe('Register and Login', () => {
         cy.get('button.btn.btn-secondary[data-dismiss="modal"]').eq(1).should('exist').and('have.text', 'Close').and('have.attr', 'type', 'button');
         // Verify and enter username
         cy.get('label[for="sign-username"].form-control-label').should('have.text', 'Username:');
+        cy.get('label[for="sign-password"].form-control-label').should('have.text', 'Password:');
         cy.wait(1000);
         cy.get('#sign-username').should('be.visible').type(userData.username);
         // Verify and enter password
-        cy.get('label[for="sign-password"].form-control-label').should('have.text', 'Password:');
         cy.wait(1000);
         cy.get('#sign-password').should('be.visible').type(userData.password);
+
         // Verify sign-up button
         cy.get('button.btn.btn-primary').eq(1).should('be.visible').and('have.text', 'Sign up').click().then(() => {
-            cy.intercept('POST', 'https://api.demoblaze.com/signup').as('signupRequest');
-            cy.wait(1000);
-            cy.wait('@signupRequest').its('response.statusCode').should('eq', 200);
+            // Intercept request [signup]
+            cy.intercept('POST', 'https://api.demoblaze.com/signup').as('signupRequest').then(()=>{
+                cy.wait(1000);
+                cy.wait('@signupRequest').its('response.statusCode').should('eq', 200);
+            })
         });
     });
+
+    it('Login && Logout', () => {
+        cy.get('#login2').should('be.visible').click();
+        cy.get('#logInModalLabel').should('be.visible').should('have.text', 'Log in');
+        cy.get('label[for="sign-username"].form-control-label').should('have.text', 'Username:');
+        cy.get('label[for="sign-password"].form-control-label').should('have.text', 'Password:');
+        // Verify and enter username
+        cy.wait(1000);
+        cy.get('#loginusername').should('be.visible').type(userData.username);
+        // Verify and enter password
+        cy.wait(1000);
+        cy.get('#loginpassword').should('be.visible').type(userData.password);
+        // Verify Login button
+        cy.contains('button', 'Log in').eq(0).click().then(() => {
+            // Intercept request [login]
+            cy.intercept('POST', 'https://api.demoblaze.com/login').as('signupRequest').then(()=>{
+                cy.wait(1000);
+                cy.wait('@signupRequest').its('response.statusCode').should('eq', 200);
+            })
+            // Verify user Name 
+            cy.get('#nameofuser').should('be.visible').should('have.text', `Welcome ${userData.username}`);
+            // Logout
+            cy.get('#logout2').should('be.visible').should('have.text', 'Log out').click()
+            cy.get('#nameofuser').should('not.be.visible');
+        })
+    })
+
 });
