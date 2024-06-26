cdbb88faa971d7627e3bab76d4f96b0057f9c9b3
mijailaoh <mijail.aoh@gmail.com>
2024-06-26 15:42:54 -0400

    Fix Test add product to car

--- cypress/e2e/003.addProducts.cy.js
+++ cypress/e2e/003.addProducts.cy.js
@@ -19,6 +19,7 @@ describe('Register and Login', () => {
 
         // Iterate over each product in productData
         productData.forEach((product) => {
+            let findStatus = false
             // Find and click on items that match the current product category
             cy.get('.list-group .list-group-item').should('be.visible').each(($item) => {
                 cy.wrap($item).invoke('text').then((text) => {
@@ -28,27 +29,50 @@ describe('Register and Login', () => {
                         cy.wrap($item).click().then(() => {
                             cy.wait(3000);
                             cy.log(`Clic en el producto: ${product.title}`);
-
-                            cy.get('#tbodyid .card-block .card-title').each(($titleElement, index, $titleElements) => {
-                                // Get the title text and compare it with the product title
-                                cy.wrap($titleElement).invoke('text').then((title) => {
-                                    const trimmedTitle = title.trim();
-                                    cy.log(`Texto del título obtenido: ${trimmedTitle}`);
-
-                                    // If the title matches the current product, click on the item
-                                    if (trimmedTitle === product.title) {
-                                        cy.wrap($titleElement).click().then(() => {
-                                            cy.wait(3000);
-                                            cy.get('.name').should('be.visible').invoke('text').should('include', product.title);
-                                            cy.get('.price-container').should('be.visible').invoke('text').should('include', `${product.price} *includes tax`);
-                                            cy.get('#myTabContent').should('be.visible').and('not.be.empty');
-                                            cy.get('.item > img').should('be.visible')
-                                            cy.contains('Add to cart').click();
-                                            cy.get('#nava').click()
-                                        });
-                                    }
+                                cy.get('#tbodyid .card-block .card-title').each(($titleElement, index, $titleElements) => {
+                                    // Get the title text and compare it with the product title
+                                    cy.wrap($titleElement).invoke('text').then((title) => {
+                                        const trimmedTitle = title.trim();
+                                        cy.log(`Texto del título obtenido: ${trimmedTitle}`);
+                                        // If the title matches the current product, click on the item
+                                        if (trimmedTitle === product.title) {
+                                            cy.wrap($titleElement).click().then(() => {
+                                                cy.wait(3000);
+                                                cy.get('.name').should('be.visible').invoke('text').should('include', product.title);
+                                                cy.get('.price-container').should('be.visible').invoke('text').should('include', `${product.price} *includes tax`);
+                                                cy.get('#myTabContent').should('be.visible').and('not.be.empty');
+                                                cy.get('.item > img').should('be.visible')
+                                                cy.contains('Add to cart').click();
+                                                cy.get('#nava').click()
+                                            });
+                                            findStatus = true
+                                        }
+                                    });
                                 });
-                            });
+                                if (findStatus != true ){
+                                    cy.get('#nava').should('be.visible').click()
+                                    cy.wait(3000);
+                                    cy.get('#tbodyid .card-block .card-title').each(($titleElement, index, $titleElements) => {
+                                        // Get the title text and compare it with the product title
+                                        cy.wrap($titleElement).invoke('text').then((title) => {
+                                            const trimmedTitle = title.trim();
+                                            cy.log(`Texto del título obtenido: ${trimmedTitle}`);
+                                            // If the title matches the current product, click on the item
+                                            if (trimmedTitle === product.title) {
+                                                cy.wrap($titleElement).click().then(() => {
+                                                    cy.wait(3000);
+                                                    cy.get('.name').should('be.visible').invoke('text').should('include', product.title);
+                                                    cy.get('.price-container').should('be.visible').invoke('text').should('include', `${product.price} *includes tax`);
+                                                    cy.get('#myTabContent').should('be.visible').and('not.be.empty');
+                                                    cy.get('.item > img').should('be.visible')
+                                                    cy.contains('Add to cart').click();
+                                                    cy.get('#nava').click()
+                                                });
+                                                findStatus = true
+                                            }
+                                        });
+                                    });
+                                }
                         });
                     }
                 });
