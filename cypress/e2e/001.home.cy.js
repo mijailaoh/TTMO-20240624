e870ffa3127527675eb86110fb3a818f49525a8c
mijailaoh <mijail.aoh@gmail.com>
2024-06-27 13:22:52 -0400

    Comentarios

--- cypress/e2e/001.home.cy.js
+++ cypress/e2e/001.home.cy.js
@@ -4,40 +4,43 @@ describe('Home Page', () => {
     cy.visit('/');
   });
 
+  // Test to verify elements on the Home Page
   it('Home Page Element', () => {
-    cy.title().should('include', 'STORE')
-    cy.contains('PRODUCT STORE').should('be.visible')
-    cy.get('#carouselExampleIndicators').should('be.visible');
-    cy.get('#navbarExample').should('be.visible');
-    cy.get('#cartur').should('be.visible');
-    cy.get('#login2').should('be.visible');
-    cy.get('#signin2').should('be.visible');
+    cy.title().should('include', 'STORE') // Check if the page title includes 'STORE'
+    cy.contains('PRODUCT STORE').should('be.visible') // Check if 'PRODUCT STORE' text is visible
+    cy.get('#carouselExampleIndicators').should('be.visible'); // Check if the carousel is visible
+    cy.get('#navbarExample').should('be.visible'); // Check if the navbar is visible
+    cy.get('#cartur').should('be.visible'); // Check if the cart icon is visible
+    cy.get('#login2').should('be.visible'); // Check if the login button is visible
+    cy.get('#signin2').should('be.visible'); // Check if the sign-up button is visible
   })
 
+  // Test to verify the Left Menu items
   it('Check Left Menu', () => {
     cy.get('.list-group .list-group-item').should('be.visible').each(($item) => {
       cy.wrap($item).invoke('text').then((text) => {
         switch (text.trim()) {
           case 'Phones':
-            cy.wrap($item).should('have.text', 'Phones');
+            cy.wrap($item).should('have.text', 'Phones'); // Check if the item text is 'Phones'
             break;
           case 'Laptops':
-            cy.wrap($item).should('have.text', 'Laptops');
+            cy.wrap($item).should('have.text', 'Laptops'); // Check if the item text is 'Laptops'
             break;
           case 'Monitors':
-            cy.wrap($item).should('have.text', 'Monitors');
+            cy.wrap($item).should('have.text', 'Monitors'); // Check if the item text is 'Monitors'
             break;
         }
       });
     });
   })
 
+  // Test to verify the Product List
   it('Checks Product List', () => {
     cy.get('#tbodyid').should('exist').each(($row) => {
-      cy.wrap($row).find('.card.h-100').should('exist');
-      cy.wrap($row).find('.card-block').should('exist');
-      cy.wrap($row).find('.card-block > .card-title').should('exist').and('not.be.empty');
-      cy.wrap($row).find('.card-block > #article').should('exist').and('not.be.empty');
+      cy.wrap($row).find('.card.h-100').should('exist'); // Check if the card element exists
+      cy.wrap($row).find('.card-block').should('exist'); // Check if the card block element exists
+      cy.wrap($row).find('.card-block > .card-title').should('exist').and('not.be.empty'); // Check if the card title exists and is not empty
+      cy.wrap($row).find('.card-block > #article').should('exist').and('not.be.empty'); // Check if the article element exists and is not empty
     });
   })
 })
--- cypress/e2e/004.viewShoppingCart.cy.js
+++ cypress/e2e/004.viewShoppingCart.cy.js
@@ -1,59 +1,58 @@
-describe('Ver carrito', () => {
+describe('View Cart', () => {
     let userData;
     let productData;
 
     beforeEach(() => {
         cy.fixture('user.json').then((data) => {
-            userData = data;
+            userData = data; // Load user data from fixture
         });
 
-        // Select products and load productData
+        // Load product data from fixture
         cy.fixture('products.json').then((data) => {
             productData = data;
         });
 
-        cy.visit('/');
+        cy.visit('/'); // Visit the home page
     });
 
-    it('Verificar Productos', () => {
+    it('Verify Products', () => {
         // Login with userData
         cy.loginUser(userData);
 
         cy.get('#cartur').should('be.visible').click().then(() => {
-            cy.wait(3000);
-            cy.screenshot('Added_Products')
-            cy.get('h2').should('be.visible').and('contain.text', 'Products');
-            cy.get('h2').should('be.visible').and('contain.text', 'Total');
-            cy.get('#totalp').should('be.visible').and('not.be.empty');
-            cy.get('button.btn.btn-success').should('exist').and('be.visible').and('have.text', 'Place Order');
-            cy.get('.table-responsive').should('be.visible').and('not.be.empty');
+            cy.wait(3000); // Wait for the products to load
+            cy.screenshot('Added_Products'); // Take a screenshot of the added products
+            cy.get('h2').should('be.visible').and('contain.text', 'Products'); // Check if the header 'Products' is visible
+            cy.get('h2').should('be.visible').and('contain.text', 'Total'); // Check if the header 'Total' is visible
+            cy.get('#totalp').should('be.visible').and('not.be.empty'); // Check if the total price element is visible and not empty
+            cy.get('button.btn.btn-success').should('exist').and('be.visible').and('have.text', 'Place Order'); // Check if the 'Place Order' button exists and is visible
+            cy.get('.table-responsive').should('be.visible').and('not.be.empty'); // Check if the table is visible and not empty
 
-            // Verifica la estructura de la tabla (thead)
+            // Verify the structure of the table header (thead)
             cy.get('thead').should('exist').within(() => {
-                cy.get('tr').should('exist'); // Verifica que haya al menos una fila dentro de <thead>
-                cy.get('th').should('have.length', 4); // Verifica que haya exactamente 4 columnas (encabezados)
-                cy.get('th').eq(0).should('have.text', 'Pic'); // Verifica que el primer encabezado sea 'Pic'
-                cy.get('th').eq(1).should('have.text', 'Title'); // Verifica que el segundo encabezado sea 'Title'
-                cy.get('th').eq(2).should('have.text', 'Price'); // Verifica que el tercer encabezado sea 'Price'
-                cy.get('th').eq(3).should('have.text', 'x'); // Verifica que el cuarto encabezado sea 'x'
+                cy.get('tr').should('exist'); // Check that there is at least one row within <thead>
+                cy.get('th').should('have.length', 4); // Check that there are exactly 4 columns (headers)
+                cy.get('th').eq(0).should('have.text', 'Pic'); // Check that the first header is 'Pic'
+                cy.get('th').eq(1).should('have.text', 'Title'); // Check that the second header is 'Title'
+                cy.get('th').eq(2).should('have.text', 'Price'); // Check that the third header is 'Price'
+                cy.get('th').eq(3).should('have.text', 'x'); // Check that the fourth header is 'x'
             });
 
-            // Verifica la cantidad y contenido de las filas dentro de tbody
-            cy.get('tbody#tbodyid').find('tr').should('have.length', productData.length); // Verifica que haya exactamente la misma cantidad de filas que productos en productData
+            // Verify the number and content of rows within tbody
+            cy.get('tbody#tbodyid').find('tr').should('have.length', productData.length); // Check that there are exactly the same number of rows as products in productData
 
             productData.forEach((product) => {
-
                 cy.get('tbody#tbodyid')
                     .find('tr')
                     .each(($row, index) => {
-                        // Dentro de cada fila ($row), accederemos a los elementos <td> y verificaremos su contenido
+                        // Within each row ($row), access the <td> elements and verify their content
                         cy.wrap($row).within(() => {
                             cy.get('td').eq(1).invoke('text').then((text) => {
                                 const titleText = text.trim();
                                 if (titleText === product.title) {
-                                    cy.get('td').eq(1).should('contain.text', product.title);
-                                    cy.get('td').eq(2).should('contain.text', product.price.toString());
-                                    cy.get('td').eq(3).should('contain.text', 'Delete');
+                                    cy.get('td').eq(1).should('contain.text', product.title); // Check if the title matches
+                                    cy.get('td').eq(2).should('contain.text', product.price.toString()); // Check if the price matches
+                                    cy.get('td').eq(3).should('contain.text', 'Delete'); // Check if the delete link is present
                                 }
                             });
                         });
@@ -62,47 +61,46 @@ describe('Ver carrito', () => {
         });
     });
 
-    it('Verificar Total Price después de eliminar un producto', () => {
+    it('Verify Total Price after Deleting a Product', () => {
         let totalPrice = 0;
-        let deletedProductPrice = 0; // Variable para almacenar el precio del producto eliminado
+        let deletedProductPrice = 0; // Variable to store the price of the deleted product
     
         // Login with userData
         cy.loginUser(userData);
     
         cy.get('#cartur').should('be.visible').click();
-        cy.wait(3000); // Esperar el tiempo necesario para que se carguen los productos en el carrito
+        cy.wait(3000); // Wait for the products to load in the cart
 
-        // Seleccionar un producto específico para eliminar (por ejemplo, el segundo producto)
-        const productToDelete = productData[1]; // Cambia el índice según tu necesidad
+        // Select a specific product to delete (e.g., the second product)
+        const productToDelete = productData[1]; // Change the index as needed
     
-        // Iterar sobre cada producto en productData y calcular la suma total de precios
+        // Iterate over each product in productData and calculate the total price
         productData.forEach((product) => {
-            totalPrice += parseInt(product.price); // Asumiendo que price es un número en formato string o número
+            totalPrice += parseInt(product.price); // Assuming price is a string or number
         });
     
-        // Verificar que el elemento #totalp esté visible y contenga el texto con la suma total de precios inicial
+        // Verify that the total price element is visible and contains the initial total price
         cy.get('#totalp').should('be.visible').and('contain.text', totalPrice.toString());
     
-        // Encontrar y hacer clic en el enlace "Delete" del producto seleccionado para eliminarlo
+        // Find and click the "Delete" link for the selected product
         cy.get('tbody#tbodyid')
-            .contains('tr', productToDelete.title) // Buscar la fila que contiene el título del producto a eliminar
+            .contains('tr', productToDelete.title) // Find the row that contains the title of the product to delete
             .within(() => {
-                cy.contains('Delete').click(); // Hacer clic en el enlace "Delete"
+                cy.contains('Delete').click(); // Click the "Delete" link
             });
     
-        // Guardar el precio del producto eliminado
+        // Store the price of the deleted product
         deletedProductPrice = parseInt(productToDelete.price);
     
-        // Recalcular el totalPrice después de eliminar el producto
+        // Recalculate the totalPrice after deleting the product
         totalPrice -= deletedProductPrice;
     
-        // Verificar que el elemento #totalp ahora contenga el nuevo totalPrice después de la eliminación
+        // Verify that the total price element now contains the new totalPrice after deletion
         cy.get('#totalp').should('be.visible').and('contain.text', totalPrice.toString());
 
-        cy.wait(2000);
-        // Verifica la cantidad y contenido de las filas dentro de tbody
-        cy.get('tbody#tbodyid').find('tr').should('have.length', 2);
-        cy.screenshot('Final_Product_List')
+        cy.wait(2000); // Wait for the updated product list to load
+        // Verify the number and content of rows within tbody
+        cy.get('tbody#tbodyid').find('tr').should('have.length', 2); // Verify that there are now 2 rows
+        cy.screenshot('Final_Product_List'); // Take a screenshot of the final product list
     });
-    
 });
